eb2bbd4a6f3f6fd7bc97ec19dbdbe2090084ca54
Anton Arzamastsev <tonich@sbor.net>
2025-11-16 22:27:06 +0300

    refactor: change structure of the project

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,5 @@
 #!/usr/bin/env node
-import { program } from 'commander'
 
-/* const command = () => {
-  console.log('Hello, World!')
-}; */
+import runGenDiff from '../src/gendiff-tool.js'
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
-//  .action(command)
-  .parse(process.argv)
+runGenDiff()
---
+++ src/gendiff-tool.js
@@ -0,0 +1,16 @@
+import { program } from 'commander'
+
+/* const command = () => {
+  console.log('Hello, World!')
+}; */
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+//  .action(command)
+  .parse(process.argv)
+
+export default program
