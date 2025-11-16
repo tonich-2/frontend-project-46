0fb79ebb6bcfc82e5429ef54f948d28eac63a4c2
Anton Arzamastsev <tonich@sbor.net>
2025-11-16 20:10:00 +0300

    feat: implement options and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,5 +9,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
 //  .action(command)
   .parse(process.argv)
