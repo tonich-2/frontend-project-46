86c7d6faef13969e8a21f9b4941931a1fb8b4064
Anton Arzamastsev <tonich@sbor.net>
2025-12-20 18:10:32 +0300

    feat: implement parsing module

---
+++ src/parsing.js
@@ -0,0 +1,22 @@
+import * as fs from 'node:fs'
+import YAML from 'yaml'
+
+const parse = (file) => {
+  const content = fs.readFileSync(file, 'utf8')
+  // console.log(typeof content)
+  const extension = file.slice(file.lastIndexOf('.') + 1)
+  // console.log(extension)
+  if (extension === 'json') {
+    const parsing = JSON.parse(content)
+    console.log(parsing) // temporary debugging printing
+    return parsing
+  }
+  else if (extension === 'yaml') {
+    const parsing = YAML.parse(content)
+    console.log(parsing) // temporary debugging printing
+    return parsing
+  }
+  throw new Error('Unsupported extension. Only json and yaml files are supported.')
+}
+
+export default parse
